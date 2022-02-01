27747546dbb5ef45011ba652757af2e97e2480db
zafra89 <zafra_joseluis@hotmail.com>
2022-02-01 19:24:56 +0100

    Added README file and some changes

---
+++ README.md
@@ -0,0 +1,15 @@
+# Jokes App frontend
+
+This project was made with simple html, js and css files.
+
+# Live Server
+
+You have to download Live Server extension from Visual Studio Code or similar extension in other editors.
+
+# Run Live Server
+
+You have to right click on index.html file and select Open with Live Server. A navigator will open with the DOM of the Jokes App.
+
+# Enjoy
+
+Now you can enjoy Jokes App by clicking for a random joke from the API and revealing the puchline of each one.
--- app.js
+++ app.js
@@ -1,27 +1,32 @@
+// DOM ELEMENTS
 const buttonDisplay = document.getElementById("buttonDisplay");
 const showPunchlineBtn = document.getElementById("showPunchlineBtn");
 const setup = document.getElementById("setup");
 const punchline = document.getElementById("punchline");
+
+// VARIABLES
 let apiData;
 let jokeId;
 
+//EVENT LISTENERS
 buttonDisplay.addEventListener('click', () => {
     getData();
 });
 
 showPunchlineBtn.addEventListener('click', () => {
     showPunchline();
-})
+});
 
+// FUNCTIONS
 function getData() {
     fetch(`http://localhost:5000/jokes`, {
         "method": "GET"
     })
     .then(res => res.json())
     .then((data, error) => {
-        if (data) {
-            let randomNumber = Math.floor(Math.random() * data.length);
+        if (data && data.length > 0) {
             apiData = data;
+            let randomNumber = Math.floor(Math.random() * data.length);
             jokeId = randomNumber;
             showRandomJoke(randomNumber);
         }
@@ -33,7 +38,6 @@ function showRandomJoke(id) {
     if (apiData && apiData.length > 0) {
         apiData.forEach(joke => {
             if (joke.id === id.toString()) {
-                // TODO: cuando nos encontremos con saltos de línea, cambiarlo
                 setup.innerText = joke.setup;
                 punchline.classList.add('hidden');
                 punchline.innerText = joke.punchline;
